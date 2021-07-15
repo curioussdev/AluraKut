7a64fe1d086c76be39c30bc20d8c514eb1b1ba36
Jose Lopes <946393361bill@gmail.com>
2021-07-15 19:06:03 +0100

    estilizando Box

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,28 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
 
-/*const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-*/
-
-const Box  = styled.div `
-background: #ffffff;
-border-radius: 8px;
-`;
-
-const MainGrid = styled.main `
-  display: grid;
-  padding: 16px;
-  grid-gap: 10px;
-
-  @media(min-width: 860px) {
-    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
-    grid-template-columns: 160px 618px 312px;
-  }
-
-  `;
 
 export default function Home() {
   return (
     <MainGrid>
-      <div style={{ gridArea: 'profileArea'}}>
+      
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
         <Box >
-          Imagem
+          <img src="https://github.com/programador-curioso.png" style={{borderRadius: '50%'}}/>
         </Box>
       </div>
-      <div>
+
+      <div className="welcomeArea">
         <Box style={{ gridArea: 'welcomeArea'}}>
           Bem-vindo
         </Box>
       </div>
-      <div>
+
+      <div className="profileRelationsArea">
         <Box style={{ gridArea: 'profileRelationsArea'}}>
           Comunidade
         </Box>
+
         <Box>
           Pessoas da comunidade
         </Box>
