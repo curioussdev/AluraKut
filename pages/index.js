ae8449cddf1fb4da01d7e946e5067a04d758bcd2
Jose Lopes <946393361bill@gmail.com>
2021-07-15 21:17:31 +0100

    testando api do github com a imagem de perfil

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
 
+function ProfileSidebar (props){
+ 
+  return(
+    <Box style={{borderRadius: '50%'}}>
+          <img src={`https://github.com/${props.githubUser}.png`} style={{borderRadius: '50%'}}/>
+    </Box>
+  );
+};
 
 export default function Home() {
+
+  const usuario = `programador-curioso`;
+
   return (
     <MainGrid>
       
       <div className="profileArea" style={{ gridArea: 'profileArea' }}>
-        <Box style={{borderRadius: '50%'}}>
-          <img src="https://github.com/programador-curioso.png" style={{borderRadius: '50%'}}/>
-        </Box>
+        <ProfileSidebar githubUser={usuario} />
+      
       </div>
 
       <div className="welcomeArea">
